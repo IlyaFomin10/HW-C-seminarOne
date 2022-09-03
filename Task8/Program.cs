3640e285c952032ea8103c436b4a72695a97003a
Ilya <fominiv10@gmail.com>
2022-09-03 16:48:40 +0300

    solved the eighth task

---
+++ Task8/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int numberWrite = 2;
+if (numberWrite <= number)
+{
+    while (numberWrite <= number)
+    {
+        Console.WriteLine(numberWrite);
+        numberWrite = numberWrite + 2;
+    }
+}
+else
+{
+    Console.WriteLine("Нет четных чисел в диапазоне от 1 до вашего числа");
+}
